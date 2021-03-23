8a8902b5685dd9a1e458e8af1bec1bb667dd0e84
tostos <saittosun2@gmail.com>
2021-03-23 03:54:03 +0100

    customers calisiyor

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -15,53 +15,37 @@ import { CustomerFacade } from '~customers/services/customer.facade';
 })
 export class DashboardComponent implements OnInit, OnDestroy {
   private destroyed$ = new Subject<boolean>();
-  customers: Lead[];
-  filteredCustomers: Lead[]
+  customers: Customer[];
+  filteredCustomers: Customer[]
 
   constructor(private store: CustomerFacade,
-              private customerService: CustomerService,
-              private storeLead: LeadFacade,
-              private leadService: LeadService) { }
+              private customerService: CustomerService) { }
 
   ngOnInit(): void {
-    // this.store.getCustomers().subscribe(customers => {
-    //   if(customers.length === 0) {
-    //     // http request
-    //     this.customerService.fetchCustomers().subscribe(customers => {
-    //       this.store.setCustomers(customers);
-    //       this.customers = customers;
-    //       this.filteredCustomers = [...this.customers];
-    //     });
-    //   } else {
-    //     this.customers = customers;
-    //     this.filteredCustomers = [...this.customers];
-    //   }
-    // })
-
-    this.storeLead.getLeads().subscribe(customers => {
-      if (customers.length === 0) {
-        this.leadService.fetchLeads().subscribe(customers => {
-          console.log(customers);
-          this.storeLead.setLeads(customers);
+    this.store.getCustomers().subscribe(customers => {
+      if(customers.length === 0) {
+        // http request
+        this.customerService.fetchCustomers().subscribe(customers => {
+          this.store.setCustomers(customers);
           this.customers = customers;
-          this.filteredCustomers = [...this.customers]
-        })
+          this.filteredCustomers = [...this.customers];
+        });
       } else {
         this.customers = customers;
-        this.filteredCustomers = [...this.customers]
+        this.filteredCustomers = [...this.customers];
       }
     })
   }
 
-  // searchThis(val: string): void {
-  //   if (val === null || val === '') {
-  //     this.filteredCustomers = [...this.customers];
-  //     return;
-  //   }
-  //   this.filteredCustomers = [...this.customers.filter(customer => {
-  //     return customer.customername.toLowerCase().includes(val.toLowerCase()) || customer.projectname.toLowerCase().includes(val.toLowerCase())
-  //   })]
-  // }
+  searchThis(val: string): void {
+    if (val === null || val === '') {
+      this.filteredCustomers = [...this.customers];
+      return;
+    }
+    this.filteredCustomers = [...this.customers.filter(customer => {
+      return customer.customername.toLowerCase().includes(val.toLowerCase()) || customer.projectname.toLowerCase().includes(val.toLowerCase())
+    })]
+  }
 
   ngOnDestroy(): void {
     this.destroyed$.next(true);
